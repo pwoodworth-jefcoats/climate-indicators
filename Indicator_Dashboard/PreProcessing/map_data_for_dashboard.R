16bc1b9fda4c43c5b7b342a868b1ef1880145232
Johanna Wren <johanna.wren@noaa.gov>
2025-04-14 15:14:17 -1000

    Commented out an made minor changes to the code to remove dealing with antimeridian in the data rasters and not using the land & coast spatial files.

--- Indicator_Dashboard/PreProcessing/map_data_for_dashboard.R
+++ Indicator_Dashboard/PreProcessing/map_data_for_dashboard.R
@@ -15,61 +15,65 @@ prep_map_data <- function(indicator, raster_ann, raster_clim, min_x, max_x, min_
   #calculate anomaly raster
   raster_anom <- raster_ann - raster_clim
   
-  #CRS to recenter for plotting
-  plot_crs <- "+proj=longlat +x_0=0 +y_0=0 +lat_0=0 +lon_0=180 +datum=WGS84 +no_defs"
-  
-  #split into two along antimeridian
-  raster_w <- raster_ann %>%
-    raster::crop(c(xmin = min_x, xmax = 180, ymin = min_y, ymax = max_y))
-  
-  raster_e <- raster_ann %>%
-    raster::crop(c(xmin = 180, xmax = max_x, ymin = min_y, ymax = max_y)) %>%
-    raster::rotate(.)
-  
-  #repeat for anomaly 
-  raster_anom_w <- raster_anom %>%
-    raster::crop(c(xmin = min_x, xmax = 180, ymin = min_y, ymax = max_y)) 
-  
-  raster_anom_e <- raster_anom %>%
-    raster::crop(c(xmin = 180, xmax = max_x, ymin = min_y, ymax = max_y)) %>%
-    raster::rotate(.) 
-  
-  #downsample if needed, otherwise just reproject
-  if (res(raster_ann)[1] < target_res) {
-    raster_w <- raster_w %>% projectRaster(., crs = plot_crs, res = target_res)
-    raster_e <- raster_e %>% projectRaster(., crs = plot_crs, res = target_res)
-    raster_anom_w <- raster_anom_w %>% projectRaster(., crs = plot_crs, res = target_res)
-    raster_anom_e <- raster_anom_e %>% projectRaster(., crs = plot_crs, res = target_res)
-  } else{
-    raster_w <- raster_w %>% projectRaster(., crs = plot_crs)
-    raster_e <- raster_e %>% projectRaster(., crs = plot_crs)
-    raster_anom_w <- raster_anom_w %>% projectRaster(., crs = plot_crs)
-    raster_anom_e <- raster_anom_e %>% projectRaster(., crs = plot_crs)
-  }
-  
-  #merge back together
-  raster_proj <- raster::merge(raster_w, raster_e, tolerance = 0.5) %>% 
-    raster::trim(.) #remove whitespace
-  
-  #repeat for anomaly map
-  raster_anom_proj <- raster::merge(raster_anom_w, raster_anom_e, tolerance = 0.5) %>% 
-    raster::trim(.) #remove whitespace
-  
+  # #CRS to recenter for plotting
+  # plot_crs <- "+proj=longlat +x_0=0 +y_0=0 +lat_0=0 +lon_0=180 +datum=WGS84 +no_defs"
+  # 
+  # #split into two along antimeridian
+  # raster_w <- raster_ann %>%
+  #   raster::crop(c(xmin = min_x, xmax = 180, ymin = min_y, ymax = max_y))
+  # 
+  # raster_e <- raster_ann %>%
+  #   raster::crop(c(xmin = 180, xmax = max_x, ymin = min_y, ymax = max_y)) %>%
+  #   raster::rotate(.)
+  # 
+  # #repeat for anomaly 
+  # raster_anom_w <- raster_anom %>%
+  #   raster::crop(c(xmin = min_x, xmax = 180, ymin = min_y, ymax = max_y)) 
+  # 
+  # raster_anom_e <- raster_anom %>%
+  #   raster::crop(c(xmin = 180, xmax = max_x, ymin = min_y, ymax = max_y)) %>%
+  #   raster::rotate(.) 
+  # 
+  # #downsample if needed, otherwise just reproject
+  # if (res(raster_ann)[1] < target_res) {
+  #   raster_w <- raster_w %>% projectRaster(., crs = plot_crs, res = target_res)
+  #   raster_e <- raster_e %>% projectRaster(., crs = plot_crs, res = target_res)
+  #   raster_anom_w <- raster_anom_w %>% projectRaster(., crs = plot_crs, res = target_res)
+  #   raster_anom_e <- raster_anom_e %>% projectRaster(., crs = plot_crs, res = target_res)
+  # } else{
+  #   raster_w <- raster_w %>% projectRaster(., crs = plot_crs)
+  #   raster_e <- raster_e %>% projectRaster(., crs = plot_crs)
+  #   raster_anom_w <- raster_anom_w %>% projectRaster(., crs = plot_crs)
+  #   raster_anom_e <- raster_anom_e %>% projectRaster(., crs = plot_crs)
+  # }
+  # 
+  # #merge back together
+  # raster_proj <- raster::merge(raster_w, raster_e, tolerance = 0.5) %>% 
+  #   raster::trim(.) #remove whitespace
+  # 
+  # #repeat for anomaly map
+  # raster_anom_proj <- raster::merge(raster_anom_w, raster_anom_e, tolerance = 0.5) %>% 
+  #   raster::trim(.) #remove whitespace
+  # 
   #convert to points for plotting
-  raster_df <- raster_proj %>%
+  raster_df <- raster_ann %>%
     rasterToPoints(.) %>% #convert to points   
     as.data.frame() %>% #convert to dataframe
-    mutate(x_disp = ifelse(x <= 0, x + 180, -(180 - x))) %>%
+    #mutate(x_disp = ifelse(x <= 0, x + 180, -(180 - x))) %>%
+    mutate(x_180 = ifelse(x > 180, x-360, x)) %>%
+    rename(x_disp=x, x=x_180) %>%  
     mutate(labels = paste0(signif(x_disp, 3), ", ", signif(y, 3),
                            "<br>", indicator,
                            ": ", signif(layer, 3))) %>%
     mutate(ID = indicator)
   
   #repeat for anomaly data
-  raster_anom_df <- raster_anom_proj %>%
+  raster_anom_df <- raster_anom %>%
     rasterToPoints(.) %>% #convert to points   
     as.data.frame() %>% #convert to dataframe
-    mutate(x_disp = ifelse(x <= 0, x + 180, -(180 - x))) %>% #recenter at 180
+    #mutate(x_disp = ifelse(x <= 0, x + 180, -(180 - x))) %>% #recenter at 180
+    mutate(x_180 = ifelse(x > 180, x-360, x)) %>%
+    rename(x_disp=x, x=x_180) %>% 
     mutate(layer_disp = ifelse(layer > 0, paste0("+ ", layer), layer)) %>% #format hovertext
     mutate(layer_disp = ifelse(layer == 0, 0, layer)) %>%
     mutate(labels = paste0(signif(x_disp, 3), ", ", signif(y, 3),
@@ -81,7 +85,7 @@ prep_map_data <- function(indicator, raster_ann, raster_clim, min_x, max_x, min_
   raster_df <- bind_rows(raster_df, raster_anom_df)
   
   #return data
-  return(list(raster_proj, raster_anom_proj, raster_df))
+  return(list(raster_ann, raster_anom, raster_df))
   
 }
 
@@ -150,79 +154,80 @@ write.csv(sst_out[[3]], here("Sea_Surface_Temperature", "SST_map_data_2024.csv")
 write.csv(chl_out[[3]], here("Ocean_Color", "Chl_map_data_2024.csv"))
 write.csv(md50_out[[3]], here("Median_Phytoplankton_Size", "Median_Phyto_map_data_2024.csv"))
 
-#get lon coordinates from cropped data
-cropped_bbox <- bbox(tatd_out[[1]])
-min_x <- cropped_bbox[1,1] 
-max_x <- cropped_bbox[1,2]
-
-#read in coast data
-world_coasts <- rnaturalearth::ne_coastline(scale = 50, returnclass = "sf") %>% st_make_valid()
-world_countries <- rnaturalearth::ne_countries(scale = 50, returnclass = "sf") %>% st_make_valid()
-
-#CRS to recenter for plotting - warnings here ok
-plot_crs <- "+proj=longlat +x_0=0 +y_0=0 +lat_0=0 +lon_0=180 +datum=WGS84 +no_defs"
-
-#base maps
-land_proj <- world_countries %>% 
-  st_break_antimeridian(lon_0 = 180) %>% #trim out dateline to avoid artifacts
-  st_transform(crs = plot_crs) %>% #adjust CRS for Pacific
-  st_crop(x = ., y = c(xmin = min_x, ymin = min_y, 
-                       xmax = max_x, ymax = max_y)) %>% #crop to area
-  st_cast(., "MULTIPOLYGON") #recast for plotting
-
-coast_proj <- world_coasts %>% 
-  st_break_antimeridian(lon_0 = 180) %>% #trim out dateline to avoid artifacts
-  st_transform(crs = plot_crs) %>% #adjust CRS for Pacific
-  st_crop(x = ., y = c(xmin = min_x, ymin = min_y, 
-                       xmax = max_x, ymax = max_y)) %>% #crop to area
-  st_cast(., "MULTILINESTRING") #recast for plotting
-
-#base maps for ENSO
-land_proj_enso <- world_countries %>% 
-  st_break_antimeridian(lon_0 = 180) %>% #trim out dateline to avoid artifacts
-  st_transform(crs = plot_crs) %>% #adjust CRS for Pacific
-  st_crop(x = ., y = c(xmin = min_x, ymin = -5, 
-                       xmax = max_x, ymax = max_y)) %>% #crop to area
-  st_cast(., "MULTIPOLYGON") #recast for plotting
-
-coast_proj_enso <- world_coasts %>% 
-  st_break_antimeridian(lon_0 = 180) %>% #trim out dateline to avoid artifacts
-  st_transform(crs = plot_crs) %>% #adjust CRS for Pacific
-  st_crop(x = ., y = c(xmin = min_x, ymin = -5, 
-                       xmax = max_x, ymax = max_y)) %>% #crop to area
-  st_cast(., "MULTILINESTRING") #recast for plotting
-
-#base maps for Tropical Cyclones
-land_proj_tcs <- world_countries %>% 
-  st_break_antimeridian(lon_0 = 180) %>% #trim out dateline to avoid artifacts
-  st_transform(crs = plot_crs) %>% #adjust CRS for Pacific
-  st_crop(x = ., y = c(xmin = -90, ymin = -50, 
-                       xmax = 100, ymax = 50)) %>% #crop to area
-  st_cast(., "MULTIPOLYGON") #recast for plotting
-
-coast_proj_tcs <- world_coasts %>% 
-  st_break_antimeridian(lon_0 = 180) %>% #trim out dateline to avoid artifacts
-  st_transform(crs = plot_crs) %>% #adjust CRS for Pacific
-  st_crop(x = ., y = c(xmin = -90, ymin = -50, 
-                       xmax = 100, ymax = 50)) %>% #crop to area
-  st_cast(., "MULTILINESTRING") #recast for plotting
-
-#quick test
-ggplot() + geom_sf(data = land_proj, color = "grey", fill = "grey") +
-  geom_sf(data = coast_proj, color = "black")
-
-ggplot() + geom_sf(data = land_proj_enso, color = "grey", fill = "grey") +
-  geom_sf(data = coast_proj_enso, color = "black")
-
-ggplot() + geom_sf(data = land_proj_tcs, color = "grey", fill = "grey") +
-  geom_sf(data = coast_proj_tcs, color = "black")
-
-#save data
-saveRDS(land_proj, "Indicator_Dashboard/Data/rnatearth_land.RData")
-saveRDS(coast_proj, "Indicator_Dashboard/Data/rnatearth_coast.RData")
- 
-saveRDS(land_proj_enso, "Indicator_Dashboard/Data/rnatearth_enso_land.RData")
-saveRDS(coast_proj_enso, "Indicator_Dashboard/Data/rnatearth_enso_coast.RData")
-
-saveRDS(land_proj_tcs, "Indicator_Dashboard/Data/rnatearth_tcs_land.RData")
-saveRDS(coast_proj_tcs, "Indicator_Dashboard/Data/rnatearth_tcs_coast.RData")
+#-------JLKW commenting this out becuase I'm having issues with these land polygons. Will use borders() in ggplot instead-----------
+# # #get lon coordinates from cropped data#get lon coordinates from cropped data
+# cropped_bbox <- bbox(tatd_out[[1]])
+# min_x <- cropped_bbox[1,1] 
+# max_x <- cropped_bbox[1,2]
+# 
+# #read in coast data
+# world_coasts <- rnaturalearth::ne_coastline(scale = 50, returnclass = "sf") %>% st_make_valid()
+# world_countries <- rnaturalearth::ne_countries(scale = 50, returnclass = "sf") %>% st_make_valid()
+# 
+# #CRS to recenter for plotting - warnings here ok
+# plot_crs <- "+proj=longlat +x_0=0 +y_0=0 +lat_0=0 +lon_0=180 +datum=WGS84 +no_defs"
+# 
+# #base maps
+# land_proj <- world_countries %>% 
+#   st_break_antimeridian(lon_0 = 180) %>% #trim out dateline to avoid artifacts
+#   st_transform(crs = plot_crs) %>% #adjust CRS for Pacific
+#   st_crop(x = ., y = c(xmin = min_x, ymin = min_y, 
+#                        xmax = max_x, ymax = max_y)) %>% #crop to area
+#   st_cast(., "MULTIPOLYGON") #recast for plotting
+# 
+# coast_proj <- world_coasts %>% 
+#   st_break_antimeridian(lon_0 = 180) %>% #trim out dateline to avoid artifacts
+#   st_transform(crs = plot_crs) %>% #adjust CRS for Pacific
+#   st_crop(x = ., y = c(xmin = min_x, ymin = min_y, 
+#                        xmax = max_x, ymax = max_y)) %>% #crop to area
+#   st_cast(., "MULTILINESTRING") #recast for plotting
+# 
+# #base maps for ENSO
+# land_proj_enso <- world_countries %>% 
+#   st_break_antimeridian(lon_0 = 180) %>% #trim out dateline to avoid artifacts
+#   st_transform(crs = plot_crs) %>% #adjust CRS for Pacific
+#   st_crop(x = ., y = c(xmin = min_x, ymin = -5, 
+#                        xmax = max_x, ymax = max_y)) %>% #crop to area
+#   st_cast(., "MULTIPOLYGON") #recast for plotting
+# 
+# coast_proj_enso <- world_coasts %>% 
+#   st_break_antimeridian(lon_0 = 180) %>% #trim out dateline to avoid artifacts
+#   st_transform(crs = plot_crs) %>% #adjust CRS for Pacific
+#   st_crop(x = ., y = c(xmin = min_x, ymin = -5, 
+#                        xmax = max_x, ymax = max_y)) %>% #crop to area
+#   st_cast(., "MULTILINESTRING") #recast for plotting
+# 
+# #base maps for Tropical Cyclones
+# land_proj_tcs <- world_countries %>% 
+#   st_break_antimeridian(lon_0 = 180) %>% #trim out dateline to avoid artifacts
+#   st_transform(crs = plot_crs) %>% #adjust CRS for Pacific
+#   st_crop(x = ., y = c(xmin = -90, ymin = -50, 
+#                        xmax = 100, ymax = 50)) %>% #crop to area
+#   st_cast(., "MULTIPOLYGON") #recast for plotting
+# 
+# coast_proj_tcs <- world_coasts %>% 
+#   st_break_antimeridian(lon_0 = 180) %>% #trim out dateline to avoid artifacts
+#   st_transform(crs = plot_crs) %>% #adjust CRS for Pacific
+#   st_crop(x = ., y = c(xmin = -90, ymin = -50, 
+#                        xmax = 100, ymax = 50)) %>% #crop to area
+#   st_cast(., "MULTILINESTRING") #recast for plotting
+# 
+# #quick test
+# ggplot() + geom_sf(data = land_proj, color = "grey", fill = "grey") +
+#   geom_sf(data = coast_proj, color = "black")
+# 
+# ggplot() + geom_sf(data = land_proj_enso, color = "grey", fill = "grey") +
+#   geom_sf(data = coast_proj_enso, color = "black")
+# 
+# ggplot() + geom_sf(data = land_proj_tcs, color = "grey", fill = "grey") +
+#   geom_sf(data = coast_proj_tcs, color = "black")
+# 
+# #save data
+# saveRDS(land_proj, "Indicator_Dashboard/Data/rnatearth_land.RData")
+# saveRDS(coast_proj, "Indicator_Dashboard/Data/rnatearth_coast.RData")
+#  
+# saveRDS(land_proj_enso, "Indicator_Dashboard/Data/rnatearth_enso_land.RData")
+# saveRDS(coast_proj_enso, "Indicator_Dashboard/Data/rnatearth_enso_coast.RData")
+# 
+# saveRDS(land_proj_tcs, "Indicator_Dashboard/Data/rnatearth_tcs_land.RData")
+# saveRDS(coast_proj_tcs, "Indicator_Dashboard/Data/rnatearth_tcs_coast.RData")
